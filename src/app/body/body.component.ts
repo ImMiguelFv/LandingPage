effbc8a345eab857b1fbd7d315829d287f748963
ImMiguelFv <mferronvinuela@gmail.com>
2024-09-09 20:26:10 +0200

    hero upload component

--- src/app/body/body.component.ts
+++ src/app/body/body.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 import { FormularioComponent } from '../formulario/formulario.component'
-
+import { HeroComponent } from '../hero/hero.component';
 @Component({
   selector: 'app-body',
   standalone: true,
-  imports: [FormularioComponent],
+  imports: [FormularioComponent, HeroComponent],
   templateUrl: './body.component.html',
   styleUrl: './body.component.css'
 })
