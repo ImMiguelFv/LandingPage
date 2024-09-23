e6d6d4450e5feaa0ae1da393d907b45453b9beeb
ImMiguelFv <mferronvinuela@gmail.com>
2024-09-23 18:47:07 +0200

    v2 del proyecto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,10 +4,11 @@ import { CommonModule } from '@angular/common';
 import { AppHeaderComponent } from './app-header/app-header.component';
 import { BodyComponent } from './body/body.component';
 import {JsonPipe} from '@angular/common'
+import { FooterComponent } from './footer/footer.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule, AppHeaderComponent, BodyComponent, JsonPipe],
+  imports: [RouterOutlet, CommonModule, AppHeaderComponent, BodyComponent, JsonPipe, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/body/body.component.ts
+++ src/app/body/body.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { FormularioComponent } from '../formulario/formulario.component'
 import { HeroComponent } from '../hero/hero.component';
+import { ProductosComponent } from '../productos/productos.component';
+import { TestimoniosComponent } from '../testimonios/testimonios.component';
+
 @Component({
   selector: 'app-body',
   standalone: true,
-  imports: [FormularioComponent, HeroComponent],
+  imports: [FormularioComponent, HeroComponent, ProductosComponent, TestimoniosComponent],
   templateUrl: './body.component.html',
   styleUrl: './body.component.css'
 })
